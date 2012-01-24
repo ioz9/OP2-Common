edb386621aab1caada5b1fa78e524d76db6303d7
gmarkall <grm08@doc.ic.ac.uk>
2012-01-24 11:41:35 +0000

    The xn argument should be OP_ALL, not 0, otherwise you only get one of the coordinates, not all of them. (Thanks to Lawrence).

--- apps/fortran/mass2d/mass.F90
+++ apps/fortran/mass2d/mass.F90
@@ -94,7 +94,7 @@ program main
   ! Assemble matrix
   call op_par_loop(mass_kernel, elements, &
        op_arg(mat, OP_ALL, elem_node, OP_ALL, elem_node, OP_INC), &
-       op_arg(xn, 0, elem_node, OP_READ))
+       op_arg(xn, OP_ALL, elem_node, OP_READ))
 
   ! Tidy up
   deallocate(p_elem_node)
